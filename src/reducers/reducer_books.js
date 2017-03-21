52e8e894803021270106bb410ad5005e30365d00
Eron <eroncastro@gmail.com>
2017-03-21 07:34:25 -0300

    Adds first reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,16 @@
+/*
+  A Reducer is a function that returns a piece of the application state.
+
+  Creating a Reducer is a two step process:
+  1) Create the reducer;
+  2) Wire it up to the application.
+*/
+
+export default function() {
+  return [
+    { title: 'JavaScript: The Good Parts' },
+    { title: 'Harry Potter' },
+    { title: 'The Dark Tower' },
+    { title: 'Eloquent Ruby' },
+  ];
+}
