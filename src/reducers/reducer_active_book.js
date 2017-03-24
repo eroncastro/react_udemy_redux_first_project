4d830d4aefbc5a907b946d69fb2ec50a39643c04
Eron <eroncastro@gmail.com>
2017-03-24 07:36:39 -0300

    Adds BookDetail container

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -3,15 +3,22 @@
   The state argument is not the application state, but the
   state the reducer is responsible for.
 
+  A reducer is going to be called whenever an action is
+  dispatched by our app. When the action is not concerned
+  to a particular reducer, we need a base case to handle it
+  and just return the current state.
+
+  Redux reducers are set up with JavaScript switch statements.
+
   It is important that we never mutate our current state
   to produce a new state. The object returned from our
-  reducer must be a new object.
+  reducer must always be a new object.
 */
 
 export default function(state = null, action) {
   switch(action.type) {
-    case 'BOOK_SELECTED':
-      return action.payload;
+  case 'BOOK_SELECTED':
+    return action.payload;
   }
 
   return state;
